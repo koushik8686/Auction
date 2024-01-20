1f2c2d5ddc4b9ccf3b3dd15a1587277a473fd5f7
koushik <pinnukoushikp@gmail.com>
2024-01-20 23:19:25 +0530

    third

--- app.js
+++ app.js
@@ -16,7 +16,15 @@ const userschema = mongoose.Schema({
     email:String,
     password:String
 })
+const itemschema= mongoose.Schema({
+    person:userschema,
+    url:String,
+    base_price:Number,
+    date:Date,
+    time:String
+})
 const usermodel = mongoose.model("userdetails",userschema)
+const itemmodel = mongoose.model("items", itemschema)
 app.post("/register", function (req, res) { 
     var email = req.body.email
     var pass = req.body.pass
@@ -25,8 +33,45 @@ app.post("/register", function (req, res) {
         password:pass
     })
     a.save()
+    console.log(a)
     res.redirect("/")
  })
+
+ app.get("/login", function (req, res) { 
+    res.sendFile(__dirname+"/views/login.html")
+  })
+
+app.post("/login" , function (req, res) { 
+    var email = req.body.email
+    var pass = req.body.pass
+    usermodel.find().then((arr)=>{
+        console.log(arr)
+        var item
+        for (let index = 0; index < arr.length; index++) {
+          if (arr[index].password==pass) {
+            item=arr[index]
+          }
+        }
+        console.log(item)
+        if (!item) {
+            res.redirect("/signup")
+        }
+        
+        if (item.password==pass) {
+            console.log(item._id)
+            res.redirect("/user/"+item._id)
+        } else {
+          res.send("wrong pass")
+        }
+    })
+ })
+ 
+app.get("/user/:email" , function (req, res) { 
+  var arr=[]
+  res.render("home", arr)
+ })
+
+
 
  app.get("/", function (req, res) { 
     res.sendFile(__dirname+"/views/intro.html")
